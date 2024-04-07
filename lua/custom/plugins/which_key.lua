46009617ad6fd117eb16cd2807d60abe67b4d358
Jakob Ziechmann <48921293+Jakob-Ziechmann@users.noreply.github.com>
2024-04-07 19:16:08 +0200

    added german special character and disabled whichkey for them

---
+++ lua/custom/plugins/which_key.lua
@@ -0,0 +1,36 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function ()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+
+  config = function ()
+    local which_key = require("which-key");
+
+    which_key.setup({
+      triggers_blacklist = {
+        i = {"a", "u", "o", "s", "A", "U", "O"}
+      }
+    });
+
+    which_key.register {
+      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+      ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+      ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+      ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+    }
+
+    -- register which-key VISUAL mode
+    -- required for visual <leader>hs (hunk stage) to work
+    which_key.register({
+      ['<leader>'] = { name = 'VISUAL <leader>' },
+      ['<leader>h'] = { 'Git [H]unk' },
+    }, { mode = 'v' })
+  end,
+}
--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -2,3 +2,11 @@
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+vim.keymap.set("i", "aeq", "ä");
+vim.keymap.set("i", "ueq", "ü");
+vim.keymap.set("i", "oeq", "ö");
+vim.keymap.set("i", "szz", "ß");
+
+vim.keymap.set("i", "Aeq", "Ä");
+vim.keymap.set("i", "Ueq", "Ü");
+vim.keymap.set("i", "Oeq", "Ö");
